48ed89a33157794bbe37b6730c91e83c7c6d6aee
erravikumar <130867166+erravikumar@users.noreply.github.com>
2023-04-16 13:27:24 +0530

    Add files via upload

---
+++ areaOfCircle.java
@@ -0,0 +1,8 @@
+public class areaOfCircle {
+    public static void main(String[] args) {
+        int r=2;
+        double pi=3.14;
+        double res=(pi*r*r);
+        System.out.println("Area of circle: "+res);
+    }
+}
